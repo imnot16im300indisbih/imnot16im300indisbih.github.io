9e2e441d7f1c90fbbda85004af963e87cd0dfa26
Coby7474 <94398327+imnot16im300indisbih@users.noreply.github.com>
2023-01-22 23:26:32 -0500

    Create index.js

---
+++ tools/emulator/index.js
@@ -0,0 +1,4 @@
+document.getElementById("overlay").style.display = "none"
+document.getElementById("page").innerHTML = (`
+<iframe style="position:absolute;border:none;width:100%;height:100%;right:0;bottom:0;" src="https://demo.emulatorjs.org/"></iframe>
+`)
