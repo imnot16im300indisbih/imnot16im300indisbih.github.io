a81a97a0e9605ea1e001daf6c626618684aa09c0
Coby7474 <94398327+imnot16im300indisbih@users.noreply.github.com>
2023-03-18 16:39:18 -0400

    Update h.js

--- pages/h.js
+++ pages/h.js
@@ -8,16 +8,22 @@ document.getElementById("page").innerHTML = `
 		<p style="font-size:30px;">Welcome to <b>Bob's Shack</b>!</p>
 	</div>
 	
-	<div class="hp-d" style="height:calc(361px + 1vw);">
+	<div class="hp-d" style="height:calc(130px + 1vw);">
 		<p style="font-size:40px;color:#0373FF;font-weight:bold;margin-bottom:0;">RANDOM FACT</p>
 		<p id="randFact" style="font-size:25px;line-height:110%;margin-top:30px;">...</p>
 	</div>
+	
+	<div class="hp-d" style="height:85px;">
+	<p style="font-size:17px;padding-bottom:6px;">©Copyright 2020-2022 Bob’s Shack</p>
+	<a style="font-size:20px;" class="tLink" onclick="showPopup('credits')">Credits</a>
+	<p style="font-size:20px;padding-top:6px;">Contact: bob@bobs-shack.xyz</p>
+</div>
 	
 </span>
 
 <span class="hp-right">
 
-	<div class="hp-d" style="height:220px;">
+	<div class="hp-d" style="height:340px;">
 	<p style="font-size:40px;color:#0373FF;font-weight:bold;">NEWS/UPDATES</p>
 		<p style="font-size:35px;font-weight:bold;margin-bottom:0;">v6.0.0 UPDATE</p>
 		<p style="font-size:25px;line-height:110%;margin-top:20px;">
@@ -30,17 +36,6 @@ document.getElementById("page").innerHTML = `
 		-Fixed Bob's Help tool<br>
 		</p>
 	</div>
-	
-	<!--<div class="hp-d" style="height:140px;">
-		<p style="font-size:40px;color:#0373FF;font-weight:bold;margin-bottom:0;">RANDOM FACT</p>
-		<p id="randFact" style="font-size:25px;line-height:110%;margin-top:30px;">...</p>
-	</div>-->
-	
-	<div class="hp-d" style="height:85px;">
-	<p style="font-size:17px;padding-bottom:6px;">©Copyright 2020-2022 Bob’s Shack</p>
-	<a style="font-size:20px;" class="tLink" onclick="showPopup('credits')">Credits</a>
-	<p style="font-size:20px;padding-top:6px;">Contact: bob@bobs-shack.xyz</p>
-</div>
 
 </span>
 
